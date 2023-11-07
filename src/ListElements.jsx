83fe758013631adbb118a8d1629555ca63cc57b1
Sanju79046 <727722euit163@skcet.ac.in>
2023-11-07 11:52:57 +0530

    Initial Commit

---
+++ src/ListElements.jsx
@@ -0,0 +1,16 @@
+import {Message} from "./Message";
+const ListElements =()=>
+{return(
+    <div>
+        <Message>  </Message>
+            <ul>
+                <li>red</li>
+                <li>blue</li>
+                <li>yellow</li>
+            </ul>
+      
+    </div>
+);
+
+}
+export {ListElements};
