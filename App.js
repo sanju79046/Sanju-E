492077c65877ed8ce96fe9f6fe7185d605f1d198
Sanju79046 <727722euit163@skcet.ac.in>
2023-11-17 09:40:53 +0530

    Another Commit

--- App.js
+++ App.js
@@ -1,11 +1,10 @@
 //import logo from './logo.svg';
 import './App.css';
-import HelloWorld from "./Day-2/HelloWorld";
+import HomePage from './HomePage/HomePage';
 function App() {
   return (
     <div>
-      <HelloWorld />
-
+      <HomePage/>
     </div>
   );
 }
